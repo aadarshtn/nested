a8b21d467c8ffd51425fae51d39f07a56ace2b53
aadarshtn <aadarshtn@gmail.com>
2020-09-18 02:44:33 +0530

    Set Up MongoDB Use Mongoose

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const expressLayout = require('express-ejs-layouts');
+const db = require('./config/mongoose');
 
 app.use(express.static('./assets'));
 app.use(expressLayout);
@@ -23,5 +24,5 @@ app.listen(8000, function(err){
     if(err){
         console.log(`Error In Running The Server : ${err}`);
     }
-    console.log(`The Server Is Up And Running At ${port} `);
+    console.log(`The Server Is Up And Running At Port :  ${port} `);
 });
