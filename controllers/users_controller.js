9df256d8908ad284714f660e65423933e0f1fffa
aadarshtn <aadarshtn@gmail.com>
2020-09-22 02:32:46 +0530

    Add SignUp Functionality

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,17 +1,17 @@
 module.exports.users = function(req,res){
-    return res.render('./users/users', {
+    return res.render('users', {
         title : "Users"
     });
 }
 
 module.exports.profile = function(req,res){
-    return res.render('./users/profile/profile', {
+    return res.render('profile', {
         title : "Profile"
     });
 }
 
 module.exports.posts = function(req,res){
-    return res.render('./users/posts/posts', {
+    return res.render('posts', {
         title : "Posts"
     });
 }
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
+const usersController = require('../controllers/users_controller');
 
 console.log('Router Loaded');
 
 router.get('/', homeController.home);
 router.get('/signup', homeController.signup);
 router.get('/login', homeController.login);
-router.use('/users', require('./users'));
+router.get('/users', usersController.users);
 router.post('/user-signup', homeController.create);
+router.get('/profile', usersController.profile);
+router.get('/posts', usersController.posts);
 
 module.exports = router;
