215cd08ab26e4ad60fc26b924632b193e7d998ce
aadarshtn <aadarshtn@gmail.com>
2020-10-01 23:58:55 +0530

    Add Dynamic Feature On Post Creation And Post Deletion

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -3,10 +3,20 @@ const Comment = require('../models/comment');
 
 module.exports.create = async function(req,res){
     try{
-        await Post.create({
+        let post = await Post.create({
             content: req.body.content,
             user: req.user._id
         });
+
+        if(req.xhr){
+            return res.status(200).json({
+                data: {
+                    post: post
+                }, 
+                message: "Post Created!!"
+            });
+        }
+
         return res.redirect('back');
     }catch(err){
         console.log('Error', err);
@@ -22,6 +32,17 @@ module.exports.destroy = async function(req,res){
         if(post.user == req.user.id){
             post.remove();
             await Comment.deleteMany({post: req.params.id});
+
+            if(req.xhr){
+                return res.status(200).json({
+                    data: {
+                        post_id: req.params.id,
+
+                    },
+                    messsage: "Post Deleted"
+                })
+                }
+
             return res.redirect('back');
         }else{
             return res.redirect('back');
